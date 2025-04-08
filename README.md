0a9d9c1c897e31094ce1d5e7674d91fe0a4d031d
Rushikesh Adhav <adhavrushikesh6@gmail.com>
2025-04-09 00:04:31 +0530

    protect routes

---
+++ README.md
@@ -0,0 +1,7 @@
+## 🧪 Postman Collection
+
+You can test the API using the provided Postman collection:
+
+🔗 [Download the collection](./postman/Task_Management_API.postman_collection.json)
+
+> Make sure to register and login to get a JWT token. Use that token in the `Authorization` header as a Bearer Token for task routes.
--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,10 +1,19 @@
-import express from 'express';
-import { createTask, getTasks, updateTask, deleteTask } from '../controllers/taskController';
+import express from "express";
+import {
+  createTask,
+  getTasks,
+  updateTask,
+  deleteTask,
+} from "../controllers/taskController";
+import { authenticateToken } from "../middlewares/auth";
+
 const router = express.Router();
 
-router.post('/', createTask);
-router.get('/', getTasks);
-router.put('/:id', updateTask);
-router.delete('/:id', deleteTask);
+router.use(authenticateToken); // protect all task routes
+
+router.post("/", createTask);
+router.get("/", getTasks);
+router.put("/:id", updateTask);
+router.delete("/:id", deleteTask);
 
 export default router;
